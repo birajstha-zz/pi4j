dede36eeaf36fa35cb91fb1f4dad7b6bc863586b
Biraj <29871174+birajstha@users.noreply.github.com>
2022-01-02 20:41:15 -0500

    Add files via upload

--- pi4j.toggle/src/main/java/com/toggle/SpringBootRunner.java
+++ pi4j.toggle/src/main/java/com/toggle/SpringBootRunner.java
@@ -2,8 +2,9 @@ package com.toggle;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class SpringBootRunner {
 
 	public static void main(String[] args) {
